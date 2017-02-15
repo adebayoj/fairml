8aa9dc38d57c8044dab71e15ee048ac0512864ce
adebayoj <julius.adebayo@gmail.com>
2017-02-14 21:18:18 -0500

    updated travis file to include coveralls testing coverage; updated testing file to check orthogonal projection. added a setup.cfg to configure pep8 and other issues.

--- fairml/tests/test_orthogonal_projection.py
+++ fairml/tests/test_orthogonal_projection.py
@@ -1,28 +1,35 @@
 import pytest
-import numpy as np 
+import numpy as np
 
 from fairml.orthogonal_projection import audit_model
 from fairml.orthogonal_projection import get_orthogonal_vector
 
-# content of test_sample.py
+
 def func(x):
     return x + 1
+
 
 def test_answer():
     assert func(3) == 4
 
-def test_constant_zero_perturbation_strategy(number_of_tries=20):
 
-    #do a few tries
+def test_orthogonal_projection(number_of_tries=20, size=10000):
+    """Orthogonal projection function. """
     for i in range(number_of_tries):
 
-        a = np.random.normal(0, 1, 10000)
-        b = np.random.binomial(10, 0.1, 10000)
+        a = np.random.normal(0, 1, size)
+        b = np.random.normal(0, 1, size)
+        c = np.random.binomial(10, 0.1, size)
+        d = np.random.uniform(0, 10, size)
 
+        # normal-normal check
         orth_b = get_orthogonal_vector(a, b)
+        assert np.dot(orth_b, a) < 1e-8
 
-        assert np.dot(orth_b, a) < 1e-6
+        # normal- normal check
+        ortho_c = get_orthogonal_vector(a, c)
+        assert np.dot(ortho_c, a) < 1e-8
 
-
-
-
+        # normal - uniform check
+        ortho_d = get_orthogonal_vector(a, d)
+        assert np.dot(ortho_d, a) < 1e-8
